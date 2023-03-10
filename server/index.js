ba98985c206b3bab7569a3acf47dcbb94c4cf566
Abhinav <rk7772534@gmail.com>
2023-03-10 09:21:17 +0530

    updated the contact me form

--- server/index.js
+++ server/index.js
@@ -14,15 +14,17 @@ const optRouter = require("./routes/forgot");
 const recipeRouter = require("./routes/recipe");
 const ratingRouter = require("./routes/rating");
 const imageRouter = require("./routes/image");
+const contactRouter = require("./routes/contact");
 
 app.use("/user", userRouter);
 app.use("/forgot", optRouter);
 app.use("/recipe", recipeRouter);
 app.use("/rating", ratingRouter);
 app.use("/image", imageRouter);
+app.use("/contact", contactRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello JS");
+  res.send("Hello Developer!");
 });
 
 app.listen(3002, () => {
