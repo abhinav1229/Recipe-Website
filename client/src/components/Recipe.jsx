e09c3843d230b21e4a24e35679e05bc37e8ae865
Abhinav <rk7772534@gmail.com>
2023-03-15 02:14:07 +0530

    edit and delete icon on hover

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -5,21 +5,26 @@ import { NavLink } from "react-router-dom";
 import Axios from "axios";
 import { BASE_URL } from "../helper/ref.js";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil, faTrash} from "@fortawesome/free-solid-svg-icons";
+
 const Recipe = (props) => {
   const { recipeName, recipeDescription, recipeId, recipeImageId } = props;
   const [imageInfo, setImageInfo] = useState({});
 
   useEffect(() => {
     Axios.post(`${BASE_URL}/image/getImage`, {
-      recipeImageId: recipeImageId
-    }).then((response) => {
-      const base64String = btoa(
-        String.fromCharCode(...new Uint8Array((response.data[0].img.data.data)))
-      )
-      setImageInfo(base64String);
-    }).catch((err) => {
-      console.log(err);
+      recipeImageId: recipeImageId,
     })
+      .then((response) => {
+        const base64String = btoa(
+          String.fromCharCode(...new Uint8Array(response.data[0].img.data.data))
+        );
+        setImageInfo(base64String);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [recipeImageId]);
 
   return (
@@ -27,9 +32,12 @@ const Recipe = (props) => {
       <NavLink to={"/aboutrecipe/" + recipeId}>
         <div className="recipe">
           <div className="imgContainer">
-            {
-              Object.keys(imageInfo).length !== 0 && <img src={`data:image/png;base64,${imageInfo}`} alt={recipeName} />
-            }
+            {Object.keys(imageInfo).length !== 0 && (
+              <img
+                src={`data:image/png;base64,${imageInfo}`}
+                alt={recipeName}
+              />
+            )}
           </div>
           <div className="contentContainer">
             <div className="recipeName">{recipeName}</div>
@@ -39,7 +47,15 @@ const Recipe = (props) => {
                 : recipeDescription.padEnd(140, "  ")}
             </p>
           </div>
-          <div className="buttonContainer"></div>
+        </div>
+        <div className="recipeHoverButtons">
+          <NavLink to={"/aboutrecipe/" + recipeId}>
+            {" "}
+            <FontAwesomeIcon icon={faPencil} />
+          </NavLink>
+          <NavLink to={"/aboutrecipe/" + recipeId}>
+            <FontAwesomeIcon icon={faTrash} />
+          </NavLink>
         </div>
       </NavLink>
     </div>
